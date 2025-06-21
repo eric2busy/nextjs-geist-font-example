fc7100f26f6742861927ce664bd4b965431a6681
eric2busy <eric2busy@proton.me>
2025-06-20 23:13:55 -0700

    Add .next/static/chunks/src_app_page_tsx_fc9c549d._.js 1750486435541

---
+++ .next/static/chunks/src_app_page_tsx_fc9c549d._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c5b0a85b._.js",
+  "static/chunks/src_a633e1c4._.js"
+],
+    source: "dynamic"
+});
